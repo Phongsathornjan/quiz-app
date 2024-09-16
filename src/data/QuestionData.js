0fd745f7794d3c49f03a64f95469b7e658723286
Phongsathornjan <Phongsathornjanjamsai@gmail.com>
2024-09-16 17:10:56 +0700

    first commit

---
+++ src/data/QuestionData.js
@@ -0,0 +1,29 @@
+const QuestionData = [
+    {
+      question: "ข้อใดไม่ใช่ tag html",
+      A: "<react>",
+      B: "<a>",
+      C: "<p>",
+      D: "<h1>",
+      answer: "A",
+    },
+    {
+      question: "ข้อใดถูกต้องเกี่ยวกับตัว React",
+      A: "ทำงานฝั่ง server",
+      B: "ใช้จัดการฐานข้อมูล",
+      C: "เป็นตัว JavaScript Library",
+      D: "ถูกทุกข้อ",
+      answer: "C",
+    },
+  
+    {
+      question: "ข้อใดไม่ใช่ Programming Language",
+      A: "PHP",
+      B: "HTML",
+      C: "Python",
+      D: "JavaScript",
+      answer: "B",
+    },
+  ];
+  
+  export default QuestionData;
